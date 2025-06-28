2a282aeab637602c0937424adfe2e0e4e0c0c401
Sergey Tretyak <sergey-grey2009@yandex.ru>
2025-06-28 17:20:04 +0300

    big piece of code...

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
-import './styles.css'
-import {ContextMenu} from './menu'
-import {TestModule} from './modules/test.module'
-import {ClicksModule} from './modules/clicks.module'
-import {ShapeModule} from './modules/shape.module'
-import { BackgroundModule } from './modules/background.module'
+import './styles.css';
+import { ContextMenu } from './menu';
+import { TestModule } from './modules/test.module';
+import { ClicksModule } from './modules/clicks.module';
+import { ShapeModule } from './modules/shape.module';
+import { BackgroundModule } from './modules/background.module';
+import { CountdownTimerModule } from './modules/timer.module';
 
-const menu = new ContextMenu('#menu')
+const menu = new ContextMenu('#menu');
 
-menu.add(new ClicksModule('clicks', 'Счетчик кликов за последние 3 секунды'))
-menu.add(new ShapeModule('shape', 'Форма'))
-menu.add(new BackgroundModule('background', 'Фоновый цвет'))
-menu.add(new TestModule('test1', 'Test 1'))
+menu.add(new ClicksModule('clicks', 'Счетчик кликов за последние 3 секунды'));
+menu.add(new ShapeModule('shape', 'Форма'));
+menu.add(new BackgroundModule('background', 'Фоновый цвет'));
+menu.add(new CountdownTimerModule('timer', 'Таймер'));
+menu.add(new TestModule('test1', 'Test 1'));
--- src/modules/test.module.js
+++ src/modules/test.module.js
@@ -1,15 +1,15 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module';
 
 export class TestModule extends Module {
-    constructor(type, text) {
-        super(type, text)
-    }
+  constructor(type, text) {
+    super(type, text);
+  }
 
-    toHTML() {
-        return `<li class="menu-item" data-type="${this.type}">${this.text}</li>`
-    }
+  toHTML() {
+    return `<li class="menu-item" data-type="${this.type}">${this.text}</li>`;
+  }
 
-    trigger() {
-        console.log('TestModule triggered')
-    }
+  trigger() {
+    console.log('TestModule triggered');
+  }
 }
