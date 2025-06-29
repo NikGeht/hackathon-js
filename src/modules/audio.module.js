7a8b554261099011fff588f6fe083fa9a3c17dcf
NikGeht <main@nikgeht.ru>
2025-06-29 20:09:42 +0700

    fix: correct auto-module

--- src/modules/audio.module.js
+++ src/modules/audio.module.js
@@ -1,9 +1,16 @@
 import {Module} from '../core/module'
 import {random} from '../utils'
 
+// Import audio files
+import audio1 from '../music/1.wav'
+import audio2 from '../music/2.wav'
+import audio3 from '../music/3.wav'
+import audio4 from '../music/4.wav'
+
 export class AudioModule extends Module {
     constructor(type, text) {
         super(type, text)
+        this.audioFiles = [audio1, audio2, audio3, audio4]
     }
 
     toHTML() {
@@ -12,9 +19,8 @@ export class AudioModule extends Module {
 
     trigger() {
         const numberMusic = random(1, 4);
-        const audio = new Audio(`../src/music/${numberMusic}.wav`);
+        const audio = new Audio(this.audioFiles[numberMusic - 1]);
         audio.play();
-        setTimeout (() => audio.load(), 2000)
-  } 
-
+        setTimeout(() => audio.load(), 2000)
+    } 
 }
